11bc94720dbbcb04bb1a200db2992545838ca416
Benjamin Antin <bantin@stanford.edu>
2022-07-22 16:51:06 -0400

    MVP of dashboard working

--- visualization/subtractr_viz.py
+++ visualization/subtractr_viz.py
@@ -45,23 +45,30 @@ import os
 
 # load results dictionary
 # use placeholder path for now
-path = "../mbcs_grids/figures/photocurrent_controls/220630_KynA/singlespot/220630_B6_Chrome2fGC8_IC_063022_Cell3_SingleSpot_Control_A_cmFormat_subtractr_caviar_results.npz"
+path = "../scripts/220704_B6_Chrome2fGC8_IC_070422_Cell1_Singlespot_Control_A_cmFormat_subtractr_caviar_results.npz"
 results = np.load(path)
 
-# reshape map_img so that powers and depths are all along the same axis,
+# reshape map_img to create a single 4D array for all maps.
+# axis 0 is power, so that we can index using power_slider value.
+# axis 1 is both maps (raw, subtracted, etc) along with z_planes
 # this is convenient for using the face_col argument of imshow
-map_img = results['demixed_map']
-num_powers, num_xs, num_ys, num_zs = map_img.shape
+maps = np.stack([results['raw_map'],
+    results['subtracted_map'], results['demixed_map']],
+    axis=0,
+)
+num_maps, num_powers, num_xs, num_ys, num_zs = maps.shape
+
+maps = np.transpose(maps, (1, 4, 0, 2, 3))
+maps = np.reshape(maps, (num_powers, num_maps * num_zs, num_xs, num_ys))
+
 unique_powers = np.unique(results['powers'])
 
 
 
-# tracefig=px.line([],title='Pixel trace')
-tracefig = make_subplots(rows=1, cols=4)
+# placeholder until first callback
+tracefig = make_subplots(rows=4, cols=1)
 tracefig.update_layout(
-    width=1000,
-    height=300,
-    margin=dict(l=10, r=0, t=30, b=10),
+    height=800,
 )
 
 app = dash.Dash(__name__)
@@ -70,82 +77,99 @@ app = dash.Dash(__name__)
 ## all functions with callbacks go here!
 
 @app.callback(
-    Output('relay-dump','children'),
+    # Output('relay-dump','children'),
     Output('graph4','figure'),
     # Output('button-value','children'),
     Input('map_fig','clickData'),
     Input('power_slider', 'value'))
 def plot_traces(clickData, value):
-    tracefig = make_subplots(rows=1, cols=4)
+    tracefig = make_subplots(rows=4, cols=1)
     if not clickData:
         raise dash.exceptions.PreventUpdate
 
-    x = clickData["points"][0]['x']
-    y = clickData["points"][0]['y']
-    plane_idx = clickData["points"][0]['curveNumber']
+    y = clickData["points"][0]['x']
+    x = clickData["points"][0]['y']
+    clicked_plot_num = clickData["points"][0]['curveNumber']
+    plane_idx = clicked_plot_num // num_maps
     window_center=(x, y)
 
+    colors = px.colors.qualitative.Dark24
     idx = 1
+
+    raw_max = np.nanmax(results['raw_tensor'][value, x, y, plane_idx, :])
+    raw_min = np.nanmin(results['raw_tensor'][value, x, y, plane_idx, :])
+    subtracted_max = np.nanmax(results['subtracted_tensor'][value, x, y, plane_idx, :])
+    subtracted_min = np.nanmin(results['subtracted_tensor'][value, x, y, plane_idx, :])
     for label in ['raw_tensor', 'est_tensor', 'subtracted_tensor', 'demixed_tensor']:
         traces = results[label][value, x, y, plane_idx, :]
         timesteps = traces.shape[-1]
-        for trace in traces:
+        for trace_idx, trace in enumerate(traces):
             tracefig.add_trace(
                 go.Scatter(
                     x=np.arange(timesteps),
                     y=trace,
-                    name='trace'
+                    line=dict(color=colors[trace_idx])
                 ),
-                row=1,
-                col=idx
+                row=idx,
+                col=1
             )
         idx += 1
+    tracefig.update_yaxes(title_text="raw", range=[raw_min, raw_max], row=1, col=1)
+    tracefig.update_yaxes(title_text="est", range=[raw_min, raw_max], row=2, col=1)
+    tracefig.update_yaxes(title_text="subtracted", range=[subtracted_min, subtracted_max], row=3, col=1)
+    tracefig.update_yaxes(title_text="demixed", range=[subtracted_min, subtracted_max], row=4, col=1)
 
     tracefig.update_layout(
-        width=1000,
-        height=300,
-        margin=dict(l=10, r=0, t=30, b=10),
+        height=800,
+        # margin=dict(l=10, r=0, t=30, b=10),
     )
-    return json.dumps(window_center), tracefig
+    return tracefig
 
 @app.callback(
     Output('map_fig','figure'),
     Input('power_slider','value'))
 def plot_maps(power_idx):
-    print(power_idx)
     map_fig=px.imshow(
-        map_img[power_idx,...],
-        facet_col=2,
+        maps[power_idx,...],
+        facet_col=0,
+        facet_col_wrap=num_maps,
         color_continuous_scale='jet',
-        title='Main image',
+        facet_col_spacing=0.03,
+        facet_row_spacing=0.03,
         zmin=0,
         zmax=20,
         origin='lower')
 
     map_fig.layout.coloraxis.showscale = True
     map_fig.update_layout(
-        margin=dict(l=20, r=20, t=30, b=0),title_x=0.5
+        height=800,
     )
     return map_fig
 map_fig = plot_maps(0) # initialize
 
 app.layout = html.Div([
+
+        # first row
         html.Div([
-            dash.dcc.Graph(id="map_fig", figure=map_fig)
-        ],
-            style={'width':'100%','display': 'inline-block','margin':'1vmax 0.5vmin 1vmax'},
-            className='four columns'),
+            # first column of first row
+            html.Div(
+                [dash.dcc.Graph(id="map_fig", figure=map_fig),],
+                style={'display': 'inline-block', 'vertical_align': 'top'},
+            ),
+            # second column of first row
+            html.Div(
+                [dash.dcc.Graph(id="graph4", figure=tracefig)], 
+                style = {'display': 'inline-block', 'vertical_align': 'top'},
+            ),
+        ]),
+
+        # second row
         html.Div(dcc.Slider(0, num_powers-1, 1,
                 id='power_slider',
-                value=0,
-        )),
-        html.Div(id='relay-dump'),
-        html.Div(
-        dash.dcc.Graph(id="graph4", figure=tracefig),style={'display': 'inline-block','margin-top':'1vmax',
-        'margin-right':'20px','width':'100%'},className='four columns')
-])
-
-
+                value=0,)
+        ),
+        # html.Div(id='relay-dump'),
+    ])
 
 # Run app and display result inline in the notebook
 if __name__ == '__main__':
