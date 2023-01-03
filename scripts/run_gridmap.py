a1c6a8402874c8dd061cf8ec0144ba4136fe80ce
Benjamin Antin <bantin@stanford.edu>
2023-01-03 14:56:33 -0500

    update run_gridmap.py for single/multi data

--- scripts/run_gridmap.py
+++ scripts/run_gridmap.py
@@ -93,27 +93,72 @@ if __name__ == "__main__":
     else:
         print('Running on targeted data. Skipping grid tensors and maps...')
 
-    # Run CAVIaR algorithm
+    # check whether the dataset has both single and multi target,
+    # and if so run CAVIaR separately on each.
+    unique_target_counts = np.unique(np.sum(results['stim_mat'] > 0, axis=0))
     if args.run_caviar:
-        N = stim_mat.shape[0]
-        model = cm.Model(N)
-        model.fit(results['demixed'],
-            stim_mat,
-            method='caviar',
-            fit_options={
-                'msrmp': args.msrmp,
-                'iters': args.iters,
-                'save_histories': args.save_histories,
-                'minimum_spike_count': args.minimum_spike_count}
-        )
+        if len(unique_target_counts) > 1:
+            print('Running CAVIaR separately on each target count...')
+            # there is an issue here where some datasets have more than 1 spot or 10 spot maps,
+            # only take the minimal or maximal number of spots present.
+            assert unique_target_counts[0] == 1, "First unique target count is not 1."
+            target_count_single = 1
+            target_count_multi = unique_target_counts[-1] # np.unique returns sorted values
+            for target_count, label in zip(
+                    [target_count_single, target_count_multi],
+                    ['single', 'multi']):
+                print('Running CAVIaR on {} target trials...'.format(target_count))
+                these_idxs = np.sum(results['stim_mat'] > 0, axis=0) == target_count
+                stim_mat = results['stim_mat'][:,these_idxs]
+                N = stim_mat.shape[0]
+                model = cm.Model(N)
+                model.fit(results['demixed'][:,these_idxs],
+                    stim_mat,
+                    method='caviar',
+                    fit_options={
+                        'msrmp': args.msrmp,
+                        'iters': args.iters,
+                        'save_histories': args.save_histories,
+                        'minimum_spike_count': args.minimum_spike_count}
+                )
+                results['model_state_' + label] = model.state
+                results['raw_' + label] = results['raw'][these_idxs]
+                results['est_' + label] = results['est'][these_idxs]
+                results['subtracted_' + label] = results['subtracted'][these_idxs]
+                results['demixed_' + label] = results['demixed'][these_idxs]
+                results['stim_mat_' + label] = results['stim_mat'][:,these_idxs]
 
-        results['model_state'] = model.state
+                # delete the original keys which are not separated by target count
+                del results['raw']
+                del results['est']
+                del results['subtracted']
+                del results['demixed']
+                del results['stim_mat']
 
+        else:
+            print('Found only %d spot data' % unique_target_counts[0])
+            if unique_target_counts[0] == 1:
+                label = 'single'
+            else:
+                label = 'multi'
+            print('Running CAVIaR on all trials (%s)...' % label)
+            N = stim_mat.shape[0]
+            model = cm.Model(N)
+            model.fit(results['demixed'],
+                stim_mat,
+                method='caviar',
+                fit_options={
+                    'msrmp': args.msrmp,
+                    'iters': args.iters,
+                    'save_histories': args.save_histories,
+                    'minimum_spike_count': args.minimum_spike_count}
+            )
+
+            results['model_state'] = model.state
+    
     # save args used to get the result
     argstr = json.dumps(args.__dict__)
     outpath = dset_name + '_' + args.file_suffix + '_results.npz'
-    np.savez(outpath, **results, args=argstr)
-
-
+    np.savez_compressed(outpath, **results, args=argstr)
 
 
