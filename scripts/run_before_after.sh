f229fabc17928a0b575939bb4c2d6c9805f318d8
Benjamin Antin <bantin@stanford.edu>
2023-02-02 19:14:36 -0500

    add .sh for before/after

---
+++ scripts/run_before_after.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#
+#SBATCH --job-name=circuitmap_grids
+#SBATCH -c 8                   
+#SBATCH --time=18:00:00             
+#SBATCH --mem-per-cpu=4GB       
+#SBATCH --gres=gpu:a40:1
+#SBATCH --output=%x.%j.out
+#SBATCH --error=%x.%j.err
+
+echo "Denoising m.s. grids"
+
+source ~/.bashrc
+conda activate opsin_subtraction_experiments
+ml cuda/11.2.0 cudnn/8.2.1.32
+
+python run_before_after_comparison.py "$@"
+
+
+# End of script
