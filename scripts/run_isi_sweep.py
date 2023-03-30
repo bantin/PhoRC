6c554aeb9af277d384e8533f2e000aeda08884cf
Benjamin Antin <bantin@stanford.edu>
2023-03-30 11:14:02 -0400

    create experiment_sim submodule
    - utilities for simulating end-to-end experiments with connectivity
    - moves some functions out of photocurrent_sim.py
    - modifies run_isi_sweep.py (changes untested)

--- scripts/run_isi_sweep.py
+++ scripts/run_isi_sweep.py
@@ -11,7 +11,7 @@ import jax.random as jrand
 from datetime import date
 from tqdm import tqdm
 
-from subtractr.photocurrent_sim import add_photocurrents_to_expt
+import subtractr.experiment_sim as expsim
 from subtractr.utils import add_subtraction_args
 from circuitmap.simulation import simulate_continuous_experiment
 
@@ -41,8 +41,11 @@ if __name__ == '__main__':
     parser.add_argument('--opsin_mean', type=float, default=0.5)
     parser.add_argument('--opsin_std', type=float, default=0.2)
     parser.add_argument('--stim_dur_ms', type=float, default=5.0)
+    parser.add_argument('--pc_response_var', type=float, default=0.01)
+    parser.add_argument('--pc_window_len_ms', type=float, default=200)
+    parser.add_argument('--sampling_freq', type=float, default=20000)
+    parser.add_argument('--stim_freq', type=float, default=30)
     parser.add_argument('--prior_context', type=int, default=100)
-    parser.add_argument('--pc_response_var', type=float, default=0.1)
     parser.add_argument('--response_length', type=int, default=900)
 
 
@@ -86,8 +89,18 @@ if __name__ == '__main__':
 
             # add photocurrents to the simulated experiment
             key = jrand.fold_in(key, i)
-            expt = add_photocurrents_to_expt(key, expt, stim_dur_ms=args.stim_dur_ms,
-                prior_context=args.prior_context, response_length=args.response_length,)
+            expt = expsim.add_photocurrents_to_expt(key, expt,
+                frac_pc_cells=args.frac_pc_cells,
+                opsin_mean=args.opsin_mean,
+                opsin_std=args.opsin_std,
+                stim_dur_ms=args.stim_dur_ms,
+                pc_response_var=args.pc_response_var,
+                pc_window_len_ms=args.pc_window_len_ms,
+                sampling_freq=args.sampling_freq,
+                stim_freq=args.stim_freq,
+                prior_context=args.prior_context,
+                response_length=args.response_length,
+                )
 
             # run subtraction
             est = subtractr.low_rank.estimate_photocurrents_by_batches(
